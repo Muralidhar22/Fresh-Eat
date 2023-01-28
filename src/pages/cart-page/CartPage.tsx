b5c7c50e1d25e0e4853697811da52fdc632fe23f
Muralidhar22 <muralidhar7794@gmail.com>
2023-01-28 15:26:18 +0530

    cart page address fix

--- src/pages/cart-page/CartPage.tsx
+++ src/pages/cart-page/CartPage.tsx
@@ -2,9 +2,9 @@ import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useCartContext } from 'contexts/cart.context';
+import { useUserContext } from 'contexts/user.context';
 import Navbar from 'components/nav/Nav.component';
 import AddressModal from './AddressModal.component';
-import { useUserContext } from 'contexts/user.context';
 import AddressForm from 'components/address-form/AddressForm.component';
 
 import { FaPlusCircle, FaTrashAlt, FaMinusCircle } from 'react-icons/fa';
@@ -18,72 +18,104 @@ const CartPage = () => {
   const navigate = useNavigate();
   const [isAddressesModalOpen, setIsAddressesModalOpen] = useState(false);
   const [isNewAddressModalOpen, setIsNewAddressModalOpen] = useState(false);
-  const deliveryAddressRef = useRef<string | null>(null);
+  const deliveryAddress = userInfo?.address.find((el) => el.isDeliveryAddress);
+  let deliveryAddressContent = null;
 
   if (cartList && cartList.length > 0) {
+    if (deliveryAddress) {
+      deliveryAddressContent = (
+        <div className={styles['address-container']}>
+          {userInfo?.address.map((option, idx) =>
+            option._id === deliveryAddress._id ? (
+              <div key={option.name + idx} className={styles['address-content']}>
+                Deliver To:{' '}
+                <span>
+                  {option.name}, {option.postalCode}
+                </span>{' '}
+                <span className={styles['address-type']}>{option.addressType}</span>
+                <div>
+                  {option.line1}, {option.city}, {option.state}
+                </div>
+              </div>
+            ) : null,
+          )}
+          <button
+            className={styles['change-address-button']}
+            onClick={() => {
+              setIsAddressesModalOpen((prev) => !prev);
+            }}
+          >
+            Change
+          </button>
+        </div>
+      );
+    } else if (userInfo?.address) {
+      deliveryAddressContent = (
+        <div className={styles['address-container']}>
+          Select a delivery address
+          <button
+            className={styles['change-address-button']}
+            onClick={() => {
+              setIsAddressesModalOpen((prev) => !prev);
+            }}
+          >
+            Select
+          </button>
+        </div>
+      );
+    } else {
+      deliveryAddressContent = (
+        <div className={styles['address-container']}>
+          Add a delivery address
+          <button
+            className={styles['change-address-button']}
+            onClick={() => {
+              setIsAddressesModalOpen((prev) => !prev);
+            }}
+          >
+            Select
+          </button>
+        </div>
+      );
+    }
+
     return (
       <>
         {cartLoader && <Loader />}
-        {isAddressesModalOpen && deliveryAddressRef.current && (
+        {isAddressesModalOpen && (
           <AddressModal
             setIsAddressesModalOpen={setIsAddressesModalOpen}
             setIsNewAddressModalOpen={setIsNewAddressModalOpen}
-            deliveryAddressId={deliveryAddressRef.current}
+            deliveryAddressId={deliveryAddress ? deliveryAddress._id : null}
           />
         )}
         {isNewAddressModalOpen && <AddressForm editMode={false} setIsAddressModalOpen={setIsNewAddressModalOpen} />}
         <Navbar />
-        <div className={styles['cart-content-container']}>
-          {userInfo?.address.map((option, idx) =>
-            option.isDeliveryAddress ? (
-              <div className={styles['address-container']} key={option.name + idx}>
-                <div className={styles['address-content']}>
-                  Deliver To:{' '}
-                  <span>
-                    {option.name}, {option.postalCode}
-                  </span>{' '}
-                  <span className={styles['address-type']}>{option.addressType}</span>
-                  <div>
-                    {option.line1}, {option.city}, {option.state}
-                  </div>
-                </div>
-                <button
-                  className={styles['change-address-button']}
-                  onClick={() => {
-                    deliveryAddressRef.current = option._id;
-                    setIsAddressesModalOpen((prev) => !prev);
-                  }}
-                >
-                  Change
+        <div className={styles['cart-list-container']}>
+          {deliveryAddressContent}
+          {cartList.map((item) => (
+            <div className={styles['cart-list-item']} key={item.product._id}>
+              <div className={styles['cart-list-item-image-container']}>
+                <img className={styles['product-image']} src={item.product.media[0].source} alt={item.product.name} />
+              </div>
+              <p>{item.product.name}</p>
+              <p>
+                <span>Price:</span> {item.product.discountPrice}
+              </p>
+              <div>
+                <button onClick={() => decreaseItemQty(item._id)}>
+                  <FaMinusCircle />
+                </button>
+                <span>{item.count}</span>
+                <button onClick={() => increaseItemQty(item._id)}>
+                  <FaPlusCircle />
                 </button>
               </div>
-            ) : null,
-          )}
-          <div className={styles['cart-list-container']}>
-            {cartList.map((item) => (
-              <div className={styles['cart-list-item']} key={item.product._id}>
-                <div className={styles['cart-list-item-image-container']}>
-                  <img className={styles['product-image']} src={item.product.media[0].source} alt={item.product.name} />
-                </div>
-                <p>{item.product.name}</p>
-                <p>
-                  <span>Price:</span> {item.product.discountPrice}
-                </p>
-                <div>
-                  <button onClick={() => decreaseItemQty(item._id)}>
-                    <FaMinusCircle />
-                  </button>
-                  <span>{item.count}</span>
-                  <button onClick={() => increaseItemQty(item._id)}>
-                    <FaPlusCircle />
-                  </button>
-                </div>
-                <button onClick={() => removeFromCart(item._id)}>
-                  <FaTrashAlt />
-                </button>
-              </div>
-            ))}
-          </div>
+              <button onClick={() => removeFromCart(item._id)}>
+                <FaTrashAlt />
+              </button>
+            </div>
+          ))}
           <div className={styles['price-container']}>
             <h2 className="text-uppercase">price details</h2>
             <ul>
